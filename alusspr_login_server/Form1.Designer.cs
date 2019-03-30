327832b3e188010d460f9c19e86aa25b3f5a8e57
Isaac Solis <Ing_Isaacsolis@hotmail.com>
2019-03-30 11:36:50 -0700

    add to the repository!

---
+++ alusspr_login_server/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace alusspr_login_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listView1.Location = new System.Drawing.Point(12, 36);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(436, 373);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.Location = new System.Drawing.Point(12, 416);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 23);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(436, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuarios";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipTitle = "Usuario Conectado";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "AlussPR Login Server";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(97, 26);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(454, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(438, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Login Log";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(454, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(438, 403);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 448);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AlussPR Login Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ alusspr_login_server/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Timers;
+using System.Diagnostics;
+
+namespace alusspr_login_server
+{
+    public partial class Form1 : Form
+    {
+        private byte[] send_data = new byte[1];
+        private byte[] data = new byte[1024];
+        private Socket s_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private List<object[]> client_sockets = new List<object[]>();
+        private bool close = true;
+        int port = 6400;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = Text + " - " + getSoftwareVersion();
+            notifyIcon1.BalloonTipClicked += NotifyIcon1_BalloonTipClicked;
+            notifyIcon1.DoubleClick += NotifyIcon1_DoubleClick;
+            this.FormClosing += Form1_FormClosing;
+            backgroundWorker1.RunWorkerCompleted += BackgroundWorker1_RunWorkerCompleted;
+            richTextBox1.TextChanged += RichTextBox1_TextChanged;
+            
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(e.Result != null)
+            {
+                string r = e.Result.ToString();
+                richTextBox1.AppendText(r);
+            }
+            s_socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            s_socket.Listen(50);
+            s_socket.BeginAccept(new AsyncCallback(acceptCallBack), null);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            this.Hide();
+            e.Cancel = close;
+        }
+
+        private void NotifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void NotifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process[] p = System.Diagnostics.Process.GetProcessesByName(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+            if (p.Length > 1)
+            {
+                MessageBox.Show("[Error] solo puede existir una instancia de este programa.", "AlussPR Login Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+            richTextBox1.Select();
+            richTextBox1.Focus();
+            InitServer();
+        }
+
+        private void InitServer()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(10000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();           
+            richTextBox1.Text = "Iniciando Servidor...\n";
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Socket socket;
+            foreach (object[] x in client_sockets)
+            {
+                socket = (Socket)x[0];
+                try
+                {
+                    send_data = Encoding.Default.GetBytes("s");
+                    socket.BeginSend(send_data, 0, send_data.Length, SocketFlags.None, new AsyncCallback(sendCallBack), socket);
+                }
+                catch (Exception)
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                    client_sockets.Remove(x);
+                    deleteItem(x[1].ToString());
+                    label1.Text = "Usuarios conectados: " + listView1.Items.Count;
+                    log(x[1].ToString(), false);
+                }
+            }
+        }
+
+        private void sendCallBack(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        private void deleteItem(string item)
+        {
+            foreach(ListViewItem x in listView1.Items)
+            {
+                if(x.Text == item)
+                {
+                    listView1.Items.Remove(x);
+                }
+            }
+        }
+
+        private void acceptCallBack(IAsyncResult AR)
+        {
+            try
+            {
+                Socket socket = s_socket.EndAccept(AR);
+                socket.BeginReceive(data, 0, data.Length, SocketFlags.None, new AsyncCallback(reciveCallBack), socket);
+                //Continue
+                s_socket.BeginAccept(new AsyncCallback(acceptCallBack), null);
+            }
+            catch (Exception)
+            {
+                //Do nothing
+            }
+        }
+
+        private void reciveCallBack(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int r = socket.EndReceive(AR);
+            string user = string.Empty;
+            if (r > 0)
+            {
+                byte[] d = new byte[r];
+                Array.Copy(data, d, r);
+                user = Encoding.Default.GetString(d);
+            }
+            if (user != string.Empty)
+            {
+                if (client_sockets.Where(x => x[1].ToString() == user).FirstOrDefault() == null)
+                {
+                    client_sockets.Add(new object[] { socket, user });
+                    if (listView1.InvokeRequired)
+                    {
+                        listView1.Invoke((MethodInvoker)delegate
+                        {
+                            listView1.Items.Add(user);
+                        });
+                    }
+                    else
+                    {
+                        listView1.Items.Add(user);
+                    }
+                    notifyIcon1.BalloonTipText = user;
+                    notifyIcon1.ShowBalloonTip(100);
+                    label1.Text = "Usuarios conectados: " + listView1.Items.Count;
+                    log(user, true);
+                }
+                else
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+        }
+
+        public void log(string user, bool connected)
+        {
+            string log = string.Empty;
+
+            if (connected)
+            {
+                log = "[" + DateTime.Now.ToString("MM/dd/yyyy h:mm tt") + "] -Conectado: " + user + "\n";
+            }
+            else
+            {
+                log = "[" + DateTime.Now.ToString("MM/dd/yyyy h:mm tt") + "] -Desconectado: " + user + "\n";
+            }
+
+            if (richTextBox1.InvokeRequired)
+            {                
+                //--------------------------------------------> Invoke
+                richTextBox1.Invoke((MethodInvoker)delegate {
+                    if ((richTextBox1.Text + log).Length >= richTextBox1.MaxLength)
+                    {
+                        richTextBox1.Clear();
+                        richTextBox1.Text = "...\n";
+                    }
+                    richTextBox1.AppendText(log);
+                });
+                //-------------------------------------------->
+            }
+            else
+            {
+                if ((richTextBox1.Text + log).Length >= richTextBox1.MaxLength)
+                {
+                    richTextBox1.Clear();
+                    richTextBox1.Text = "...\n";
+                }
+                richTextBox1.AppendText(log);
+            }
+        }
+
+        public string getSoftwareVersion()
+        {
+            try
+            {
+                return FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion;
+            }
+            catch (Exception)
+            {
+                return "0.0.0.0";
+            }
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            close = false;
+            this.Close();
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://clients3.google.com/generate_204"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (CheckForInternetConnection())
+            {
+                e.Result = "Servidor Iniciado (Puerto: " + port + ")...\n";
+            }
+            else
+            {
+                e.Result = "Servidor Iniciado (Puerto: " + port + ")(Aviso: se detectarón problemas de conexión)...\n";
+            }
+        }
+    }
+}
